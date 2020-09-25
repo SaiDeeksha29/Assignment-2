f1387b4b7539c9ae0bb42a6fabcda56d401a4a19
SaiDeeksha29 <saideeksha29@gmail.com>
2020-09-25 20:24:42 +0530

    Added:Welcome Message

---
+++ LineComparison.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.assignmentThree;
+
+/**
+ * @author saideeksha
+ *
+ */
+public class LineComparison {
+
+	/**
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Welcome message
+		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+	}
+}
